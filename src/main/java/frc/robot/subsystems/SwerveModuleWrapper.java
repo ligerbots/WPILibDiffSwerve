e4fd8408bd083a400317dfd9625a7a991252e931
LQ1234 <larryqiu424@yahoo.com>
2021-11-08 18:23:13 -0500

    Initial swervemodule constructor + SensorTransmissionConfiguration alternate constructor

--- src/main/java/frc/robot/subsystems/SwerveModuleWrapper.java
+++ src/main/java/frc/robot/subsystems/SwerveModuleWrapper.java
@@ -5,13 +5,32 @@
 package frc.robot.subsystems;
 
 
+import com.electronwill.nightconfig.core.Config;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import frc.team88.swerve.configuration.subconfig.Falcon500Configuration;
+import frc.team88.swerve.configuration.subconfig.SensorTransmissionConfiguration;
+import frc.team88.swerve.configuration.subconfig.SwerveModuleConfiguration;
 import frc.team88.swerve.module.SwerveModule;
+import frc.team88.swerve.module.motor.Falcon500;
+import frc.team88.swerve.module.motor.SwerveMotor;
+import frc.team88.swerve.module.sensor.PositionSensor;
+import frc.team88.swerve.module.sensor.SensorTransmission;
+import frc.team88.swerve.module.sensor.SwerveCANcoder;
 
 public class SwerveModuleWrapper {
-  
-  public SwerveModuleWrapper(int driveMotorChannel, int turningMotorChannel) {
 
+  public SwerveModuleWrapper(int canID0, int canID1, int canIDAzimuth, double offsetAzmuth) {
+    Falcon500Configuration falconConfig = new Falcon500Configuration(false, 101.0);
+    SwerveMotor motors[] =
+            new SwerveMotor[] {
+                    new Falcon500(canID0, falconConfig), // motors/0
+                    new Falcon500(canID1, falconConfig)  // motors/1
+            };
+    PositionSensor azimuthSensor =
+            new SensorTransmission(new SwerveCANcoder(canIDAzimuth), new SensorTransmissionConfiguration(false,1,offsetAzmuth));
+    SwerveModuleConfiguration swerveModuleConfig = new SwerveModuleConfiguration();
+
+    SwerveModule swerveModule = new SwerveModule(motors, azimuthSensor, swerveModuleConfig);
   }
 
   /**
