bfee50bd1c0cc7be4b9e574ee17e172af419829a
Paul Rensing <prensing@ligerbots.org>
2021-11-24 09:20:02 -0500

    Put in some debugging code
    Create periodic() method for Drivetrain
     - update odometry every loop
     - for debug purposes, have each swerve module update NT with current values
    Code formatting and cleanup

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,121 +11,109 @@ import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj.util.Units;
-import edu.wpi.first.wpiutil.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Represents a swerve drive style drivetrain. */
-public class Drivetrain {
-  public static final double kMaxSpeed = Units.feetToMeters(14.7); // 14.7 feet per second
-  public static final double kMaxSpeedTurtle = Units.feetToMeters(5.0); // 5.0 feet per second
-  public static final double kMaxAngularSpeed = 2*Math.PI; // rotations per second
+public class Drivetrain extends SubsystemBase {
+    private static final double kMaxSpeed = Units.feetToMeters(14.7); // 14.7 feet per second
+    private static final double kMaxSpeedTurtle = Units.feetToMeters(5.0); // 5.0 feet per second
+    private static final double kMaxAngularSpeed = 2 * Math.PI; // rotations per second
 
-  private static final double offsetX = 11.075;
-  private static final double offsetY = 12.575;
-  /*From swerve.toml*/
-  private final Translation2d m_frontLeftLocation = new Translation2d(Units.inchesToMeters(offsetX), Units.inchesToMeters(offsetY));
-  private final Translation2d m_frontRightLocation = new Translation2d(Units.inchesToMeters(offsetX), Units.inchesToMeters(-offsetY));
-  private final Translation2d m_backLeftLocation = new Translation2d(Units.inchesToMeters(-offsetX), Units.inchesToMeters(offsetY));
-  private final Translation2d m_backRightLocation = new Translation2d(Units.inchesToMeters(-offsetX), Units.inchesToMeters(-offsetY));
+    private static final double offsetX = 11.075;
+    private static final double offsetY = 12.575;
 
-  private final SwerveModuleWrapper m_frontLeft = new SwerveModuleWrapper(
-          12, 11, 10, 42.7, offsetX, offsetY
-  );
-  private final SwerveModuleWrapper m_frontRight = new SwerveModuleWrapper(
-          9,8,7, 3.07, offsetX, -offsetY
-  );
-  private final SwerveModuleWrapper m_backLeft = new SwerveModuleWrapper(
-          3,2,1,-52.38, -offsetX, offsetY
-  );
-  private final SwerveModuleWrapper m_backRight = new SwerveModuleWrapper(
-          6,5,4, -50.54, -offsetX, -offsetY
-  );
+    /* From swerve.toml */
+    private final Translation2d m_frontLeftLocation = new Translation2d(Units.inchesToMeters(offsetX), Units.inchesToMeters(offsetY));
+    private final Translation2d m_frontRightLocation = new Translation2d(Units.inchesToMeters(offsetX), Units.inchesToMeters(-offsetY));
+    private final Translation2d m_backLeftLocation = new Translation2d(Units.inchesToMeters(-offsetX), Units.inchesToMeters(offsetY));
+    private final Translation2d m_backRightLocation = new Translation2d(Units.inchesToMeters(-offsetX), Units.inchesToMeters(-offsetY));
 
-  private final AnalogGyro m_gyro = new AnalogGyro(0);
+    private final SwerveModuleWrapper m_frontLeft = new SwerveModuleWrapper("frontLeft", 12, 11, 10, 42.7, offsetX, offsetY);
+    private final SwerveModuleWrapper m_frontRight = new SwerveModuleWrapper("frontRight", 9, 8, 7, 3.07, offsetX, -offsetY);
+    private final SwerveModuleWrapper m_backLeft = new SwerveModuleWrapper("backLeft", 3, 2, 1, -52.38, -offsetX, offsetY);
+    private final SwerveModuleWrapper m_backRight = new SwerveModuleWrapper("backRight", 6, 5, 4, -50.54, -offsetX, -offsetY);
 
-  private final SwerveDriveKinematics m_kinematics =
-      new SwerveDriveKinematics(
-          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+    private final AnalogGyro m_gyro = new AnalogGyro(0);
 
-  private final SwerveDriveOdometry m_odometry =
-      new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d());
+    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(m_frontLeftLocation,
+            m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
 
-  // Control Objects
-  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+    private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d());
 
+    // Control Objects
+    private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
+    private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
+    private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
 
-  public Drivetrain() {
-    m_gyro.reset();
-  }
-
-  /**
-   * Method to drive the robot using joystick info.
-   *
-   * @param xSpeed Speed of the robot in the x direction (forward).
-   * @param ySpeed Speed of the robot in the y direction (sideways).
-   * @param rot Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
-   */
-  @SuppressWarnings("ParameterName")
-  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-
-    var swerveModuleStates =
-        m_kinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
-                : new ChassisSpeeds(xSpeed, ySpeed, rot));
-    SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, kMaxSpeed);
-    m_frontLeft.setDesiredState(swerveModuleStates[0]);
-    m_frontRight.setDesiredState(swerveModuleStates[1]);
-    m_backLeft.setDesiredState(swerveModuleStates[2]);
-    m_backRight.setDesiredState(swerveModuleStates[3]);
-  }
-
-  public void driveWithJoystick(double controllerx, double controllery, double controllerrotation, boolean turtlemode, boolean fieldRelative) {
-    double maxSpeed= turtlemode ? Drivetrain.kMaxSpeedTurtle : Drivetrain.kMaxSpeed;
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-
-    final var xSpeed =
-            -m_xspeedLimiter.calculate(applyDeadband(controllery, 0.02))
-                    * maxSpeed;
-
-    // Get the y speed or sideways/strafe speed. We are inverting this because
-    // we want a positive value when we pull to the left. Xbox controllers
-    // return positive values when you pull to the right by default.
-    final var ySpeed =
-            -m_yspeedLimiter.calculate(applyDeadband(controllerx, 0.02))
-                    * maxSpeed;
-
-    // Get the rate of angular rotation. We are inverting this because we want a
-    // positive value when we pull to the left (remember, CCW is positive in
-    // mathematics). Xbox controllers return positive values when you pull to
-    // the right by default.
-    final var rot =
-            -m_rotLimiter.calculate(applyDeadband(controllerrotation, 0.02))
-                    * Drivetrain.kMaxAngularSpeed;
-
-    this.drive(xSpeed, ySpeed, rot, fieldRelative);
-  }
-  private static double applyDeadband(double value, double deadband) {
-    if (Math.abs(value) > deadband) {
-      if (value > 0.0) {
-        return (value - deadband) / (1.0 - deadband);
-      } else {
-        return (value + deadband) / (1.0 - deadband);
-      }
-    } else {
-      return 0.0;
+    public Drivetrain() {
+        m_gyro.reset();
     }
-  }
-  /** Updates the field relative position of the robot. */
-  public void updateOdometry() {
-    m_odometry.update(
-        m_gyro.getRotation2d(),
-        m_frontLeft.getState(),
-        m_frontRight.getState(),
-        m_backLeft.getState(),
-        m_backRight.getState());
-  }
+
+    @Override
+    public void periodic() {
+        m_odometry.update(m_gyro.getRotation2d(), m_frontLeft.getState(), m_frontRight.getState(),
+                m_backLeft.getState(), m_backRight.getState());
+
+        // for debug, have the modules populate NetworkTables
+        m_frontLeft.populateNetworkTable();
+        m_frontRight.populateNetworkTable();
+        m_backLeft.populateNetworkTable();
+        m_backRight.populateNetworkTable();
+    }
+
+    /**
+     * Method to drive the robot using joystick info.
+     *
+     * @param xSpeed        Speed of the robot in the x direction (forward).
+     * @param ySpeed        Speed of the robot in the y direction (sideways).
+     * @param rot           Angular rate of the robot.
+     * @param fieldRelative Whether the provided x and y speeds are relative to the
+     *                      field.
+     */
+    // @SuppressWarnings("ParameterName")
+    public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+
+        var swerveModuleStates = m_kinematics.toSwerveModuleStates(
+                fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+                        : new ChassisSpeeds(xSpeed, ySpeed, rot));
+        SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, kMaxSpeed);
+        m_frontLeft.setDesiredState(swerveModuleStates[0]);
+        m_frontRight.setDesiredState(swerveModuleStates[1]);
+        m_backLeft.setDesiredState(swerveModuleStates[2]);
+        m_backRight.setDesiredState(swerveModuleStates[3]);
+    }
+
+    public void driveWithJoystick(double controllerx, double controllery, double controllerrotation, boolean turtlemode,
+            boolean fieldRelative) {
+        double maxSpeed = turtlemode ? kMaxSpeedTurtle : kMaxSpeed;
+        // Get the x speed. We are inverting this because Xbox controllers return
+        // negative values when we push forward.
+
+        final var xSpeed = -m_xspeedLimiter.calculate(applyDeadband(controllery, 0.02)) * maxSpeed;
+
+        // Get the y speed or sideways/strafe speed. We are inverting this because
+        // we want a positive value when we pull to the left. Xbox controllers
+        // return positive values when you pull to the right by default.
+        final var ySpeed = -m_yspeedLimiter.calculate(applyDeadband(controllerx, 0.02)) * maxSpeed;
+
+        // Get the rate of angular rotation. We are inverting this because we want a
+        // positive value when we pull to the left (remember, CCW is positive in
+        // mathematics). Xbox controllers return positive values when you pull to
+        // the right by default.
+        final var rot = -m_rotLimiter.calculate(applyDeadband(controllerrotation, 0.02)) * kMaxAngularSpeed;
+
+        this.drive(xSpeed, ySpeed, rot, fieldRelative);
+    }
+
+    private static double applyDeadband(double value, double deadband) {
+        if (Math.abs(value) > deadband) {
+            if (value > 0.0) {
+                return (value - deadband) / (1.0 - deadband);
+            } else {
+                return (value + deadband) / (1.0 - deadband);
+            }
+        } else {
+            return 0.0;
+        }
+    }
 }
--- src/main/java/frc/robot/subsystems/SwerveModuleWrapper.java
+++ src/main/java/frc/robot/subsystems/SwerveModuleWrapper.java
@@ -4,14 +4,17 @@
 
 package frc.robot.subsystems;
 
-
 import com.electronwill.nightconfig.core.Config;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.util.Units;
 import frc.team88.swerve.configuration.subconfig.Falcon500Configuration;
 import frc.team88.swerve.configuration.subconfig.SensorTransmissionConfiguration;
 import frc.team88.swerve.configuration.subconfig.SwerveModuleConfiguration;
+import frc.team88.swerve.data.representations.SwerveModuleData;
 import frc.team88.swerve.module.SwerveModule;
 import frc.team88.swerve.module.motor.Falcon500;
 import frc.team88.swerve.module.motor.SwerveMotor;
@@ -24,100 +27,98 @@ import java.util.List;
 
 public class SwerveModuleWrapper {
 
-  SwerveModule swerveModule;
-  public SwerveModuleWrapper(
-          int canID0,
-          int canID1,
-          int canIDAzimuth,
-          double offsetAzimuth,
-          double location_x,
-          double location_y
-  ) {
-    Falcon500Configuration falconConfig = new Falcon500Configuration(
-            makeFalcon500Config(false, 101.0)
-    );
+    SwerveModule m_swerveModule;
+    String m_name;
 
-    SwerveMotor motors[] =
-            new SwerveMotor[] {
-                    new Falcon500(canID0, falconConfig), // motors/0
-                    new Falcon500(canID1, falconConfig)  // motors/1
-            };
-    PositionSensor azimuthSensor =
-            new SensorTransmission(
-                    new SwerveCANcoder(canIDAzimuth),
-                    new SensorTransmissionConfiguration(
-                            makeSensorTransmissionConfig(false,1,offsetAzimuth)
-                    )
-            );
+    public SwerveModuleWrapper(String name, int canID0, int canID1, int canIDAzimuth, double offsetAzimuth,
+                               double location_x, double location_y) 
+    {
+        m_name = name;
+        Falcon500Configuration falconConfig = new Falcon500Configuration(makeFalcon500Config(false, 101.0));
 
-    Config config = Config.inMemory();
-    config.set("location-inches.x", location_x);
-    config.set("location-inches.y", location_y);
-    config.set("differential-matrix", List.of(
-            List.of(0.041666666, -0.041666666),
-            List.of(0.069444444, 0.09722222)
-    ));
-    config.set("wheel-diameter-inches",3.5);
+        SwerveMotor motors[] = new SwerveMotor[] { 
+                new Falcon500(canID0, falconConfig), // motors/0
+                new Falcon500(canID1, falconConfig) // motors/1
+        };
+        PositionSensor azimuthSensor = new SensorTransmission(new SwerveCANcoder(canIDAzimuth),
+                new SensorTransmissionConfiguration(makeSensorTransmissionConfig(false, 1, offsetAzimuth)));
 
-    config.set("azimuth-controller.kP", 20.0);
-    config.set("azimuth-controller.kI", 0.0);
-    config.set("azimuth-controller.kD", 0.0);
-    config.set("azimuth-controller.kF", 0.0);
-    config.set("azimuth-controller.i-zone", 0.0);
-    config.set("azimuth-controller.i-max", 0.0);
-    config.set("azimuth-controller.max-speed", 1440);
-    config.set("azimuth-controller.max-acceleration", 1080);
+        Config config = Config.inMemory();
+        config.set("location-inches.x", location_x);
+        config.set("location-inches.y", location_y);
+        config.set("differential-matrix",
+                List.of(List.of(0.041666666, -0.041666666), List.of(0.069444444, 0.09722222)));
+        config.set("wheel-diameter-inches", 3.5);
 
-    config.set("wheel-controller.kP", 0.0);
-    config.set("wheel-controller.kI", 0.1);
-    config.set("wheel-controller.kD", 0.0);
-    config.set("wheel-controller.i-zone", 1.5);
-    config.set("wheel-controller.i-max", 0.0);
+        config.set("azimuth-controller.kP", 20.0);
+        config.set("azimuth-controller.kI", 0.0);
+        config.set("azimuth-controller.kD", 0.0);
+        config.set("azimuth-controller.kF", 0.0);
+        config.set("azimuth-controller.i-zone", 0.0);
+        config.set("azimuth-controller.i-max", 0.0);
+        config.set("azimuth-controller.max-speed", 1440);
+        config.set("azimuth-controller.max-acceleration", 1080);
 
-    SwerveModuleConfiguration swerveModuleConfig = new SwerveModuleConfiguration(config);
+        config.set("wheel-controller.kP", 0.0);
+        config.set("wheel-controller.kI", 0.1);
+        config.set("wheel-controller.kD", 0.0);
+        config.set("wheel-controller.i-zone", 1.5);
+        config.set("wheel-controller.i-max", 0.0);
 
-    swerveModule = new SwerveModule(motors, azimuthSensor, swerveModuleConfig);
-  }
+        SwerveModuleConfiguration swerveModuleConfig = new SwerveModuleConfiguration(config);
 
-  private Config makeFalcon500Config(boolean inverted, double maxSpeed){
-    Config config = Config.inMemory();
-    config.set("inverted", inverted);
-    config.set("max-speed-rps", maxSpeed);
-    return config;
-  }
-  private Config makeSensorTransmissionConfig(boolean inverted, double ratio, double offset){
-    Config config = Config.inMemory();
-    config.set("inverted", inverted);
-    config.set("ratio", ratio);
-    config.set("offset", offset);
-    return config;
-  }
-  /**
-   * Returns the current state of the module.
-   *
-   * @return The current state of the module.
-   */
-  public SwerveModuleState getState() {
-    // getWheelVelocity returns speed in feet per second but SwerveModuleState wants meters per second
-    // WrappedAngle.asDouble returns angle in degrees
-    SwerveModuleState swerveModuleState = new SwerveModuleState(
-            Units.feetToMeters(swerveModule.getWheelVelocity()),
-            Rotation2d.fromDegrees(swerveModule.getAzimuthPosition().asDouble())
-    );
+        m_swerveModule = new SwerveModule(motors, azimuthSensor, swerveModuleConfig);
+    }
 
-    return swerveModuleState;
-  }
+    private Config makeFalcon500Config(boolean inverted, double maxSpeed) {
+        Config config = Config.inMemory();
+        config.set("inverted", inverted);
+        config.set("max-speed-rps", maxSpeed);
+        return config;
+    }
 
-  /**
-   * Sets the desired state for the module.
-   *
-   * @param desiredState Desired state with speed and angle.
-   */
-  public void setDesiredState(SwerveModuleState desiredState) {
-    // wheelVelocity is in feet per second
-    double wheelVelocity = Units.metersToFeet(desiredState.speedMetersPerSecond);
-    // WrappedAngle accepts degrees in constructor
-    WrappedAngle azimuthPosition = new WrappedAngle(desiredState.angle.getDegrees());
-    swerveModule.set(wheelVelocity, azimuthPosition);
-  }
+    private Config makeSensorTransmissionConfig(boolean inverted, double ratio, double offset) {
+        Config config = Config.inMemory();
+        config.set("inverted", inverted);
+        config.set("ratio", ratio);
+        config.set("offset", offset);
+        return config;
+    }
+
+    /**
+     * Returns the current state of the module.
+     *
+     * @return The current state of the module.
+     */
+    public SwerveModuleState getState() {
+        // getWheelVelocity returns speed in feet per second but SwerveModuleState wants
+        // meters per second
+        // WrappedAngle.asDouble returns angle in degrees
+        SwerveModuleState swerveModuleState = new SwerveModuleState(
+                Units.feetToMeters(m_swerveModule.getWheelVelocity()),
+                Rotation2d.fromDegrees(m_swerveModule.getAzimuthPosition().asDouble()));
+
+        return swerveModuleState;
+    }
+
+    /**
+     * Sets the desired state for the module.
+     *
+     * @param desiredState Desired state with speed and angle.
+     */
+    public void setDesiredState(SwerveModuleState desiredState) {
+        // wheelVelocity is in feet per second
+        double wheelVelocity = Units.metersToFeet(desiredState.speedMetersPerSecond);
+        // WrappedAngle accepts degrees in constructor
+        WrappedAngle azimuthPosition = new WrappedAngle(desiredState.angle.getDegrees());
+        m_swerveModule.set(wheelVelocity, azimuthPosition);
+    }
+
+    // Populate NetworkTables with the current setttings. Use the TJ^2 system.
+    public void populateNetworkTable() {
+        SwerveModuleData data = new SwerveModuleData(m_swerveModule);
+        NetworkTable mainTable = NetworkTableInstance.getDefault().getTable("swerveLibrary");
+
+        data.populateNetworkTable(mainTable.getSubTable("modules").getSubTable(m_name));
+    }
 }
